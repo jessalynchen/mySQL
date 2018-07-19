134993c5af7ba10abf5707f8c9335d47d879e9b8
jessalyn <35002728+jessalynchen@users.noreply.github.com>
2018-07-19 18:38:32 -0400

    Add files via upload

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,43 @@
 //display all the items aailable for sale
-//install npm cli-table, mysql
-var mysql = require(mySQL);
-var cli = require(cliTable);
+//install npm cli-table, mysql, inquire package later
+var mysql = require("mysql");
+var cliTable = require("cli-table");
+
+var table = new cliTable({
+    head: ['item_id', 'product_name', 'department_name', 'price', 'stock_quantity'],
+   colWidths: [15, 20, 20, 20, 20]
+});
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "jessalyn",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+});
+
+
+
+function afterConnection() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+       // console.log(res)
+        for (i = 0; i < res.length; i++) {
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+           
+        }
+        console.log(table.toString());
+        connection.end();
+        console.log("it works!");
+    });
+}//res[i].columnname, rest[i].columnname2,...
+
 // Then create a Node application called bamazonCustomer.js. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
 // The app should then prompt users with two messages.
 
